1566975689a92ba51e0c0db358a8a215bf41b320
bianyunjian <byj626680108@gmail.com>
2023-07-05 11:12:40 +0800

    update domain

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-window.BASE_API = "https://web3go.xyz/backconfig-api";
-window.IMG_PREFIX = 'https://web3go.xyz/resources/images'
+window.BASE_API = "https://app.web3go.xyz/backconfig-api";
+window.IMG_PREFIX = 'https://app.web3go.xyz/resources/images'
 window.MOBILE_URL = 'https://m.web3go.xyz/'
